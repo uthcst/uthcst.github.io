d9b05ad5ef4572e67e9dc58d0d400068d3bee512
tzigri <tzihgri@gmail.com>
2022-12-11 17:56:45 +0200

    commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 4000;
+app.use(express.static(__dirname + '/'));
+const server = app.listen(4000, function () {
+  console.log("Server listening at port " + port);
+})
